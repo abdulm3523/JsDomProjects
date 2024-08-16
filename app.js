8cba33edabab90a7dc11505fbe2618a9832e3be5
abdulm3523 <abdulmalik3523@gmail.com>
2024-08-16 13:31:12 +0300

    updated

--- app.js
+++ app.js
@@ -7,18 +7,18 @@ const changeBgRgb = document.getElementById('generate__background_rgb')
 const valuePrintRgb = document.getElementById('valuePrintRgb')
 
 // One click to change background color
-genBgBtnRgb.addEventListener('click',function(){
+genBgBtnRgb.addEventListener('click', function () {
     const bgRgb = genRandomBgRgb()
     changeBgRgb.style.backgroundColor = bgRgb
     valuePrintRgb.innerHTML = genRandomBgRgb()
 
 })
 // Generating randomcolor
-function genRandomBgRgb(){
+function genRandomBgRgb() {
     // Creating rgb color RED, GREEN BLUE MAXIMUM COLOR CALUE IS 255
-    const red = Math.floor(Math.random()*255)
-    const green = Math.floor(Math.random()*255)
-    const blue = Math.floor(Math.random()*255)
+    const red = Math.floor(Math.random() * 255)
+    const green = Math.floor(Math.random() * 255)
+    const blue = Math.floor(Math.random() * 255)
 
     return `rgb(${red},${green},${blue})`
 }
@@ -34,7 +34,7 @@ const genBgBtnHex = document.getElementById('genBgBtnHex')
 const changeBgHex = document.getElementById('generate__background_hex')
 const valuePrintHex = document.getElementById('valuePrintHex')
 // One click to change background color
-genBgBtnHex.addEventListener('click',function(){
+genBgBtnHex.addEventListener('click', function () {
     const bgHex = genRandomBgHex()
     changeBgHex.style.backgroundColor = bgHex
     valuePrintHex.value = genRandomBgHex()
@@ -44,33 +44,34 @@ genBgBtnHex.addEventListener('click',function(){
 })
 
 // Generating hex randomcolor
-function genRandomBgHex(){
+function genRandomBgHex() {
     // Creating rgb color RED, GREEN BLUE MAXIMUM COLOR CALUE IS 255
-    const red = Math.floor(Math.random()*255)
-    const green = Math.floor(Math.random()*255)
-    const blue = Math.floor(Math.random()*255)
+    const red = Math.floor(Math.random() * 255)
+    const green = Math.floor(Math.random() * 255)
+    const blue = Math.floor(Math.random() * 255)
     // converted to the haxdecimal code 16
     return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`
-   
+
 }
 
 // ==================================================
 // One Click to copy text
 const copyBtn = document.getElementById('copyCode')
 const copyTextAlart = document.getElementById('textDisplay')
-copyBtn.addEventListener('click', function(){
+copyBtn.addEventListener('click', function () {
+    // window navigator copy clopboard
     navigator.clipboard.writeText(valuePrintHex.value)
     const span = document.createElement('span')
     span.id = 'textDisplay'
-    
+
 
     // Validation the code before copy
-    if(valuePrintHex.value && isValidColor(valuePrintHex.value)){
+    if (valuePrintHex.value && isValidColor(valuePrintHex.value)) {
         span.innerText = `${valuePrintHex.value} Copied!`
         changeBgRgb.appendChild(span)
     }
     // Remove tha span after click
-    span.addEventListener('click',function(){
+    span.addEventListener('click', function () {
         this.remove()
     })
 })
@@ -82,14 +83,14 @@ copyBtn.addEventListener('click', function(){
 // =================
 // taking value from input filed then check is that valid if valid change the values
 
-valuePrintHex.addEventListener('keyup',function(e){
+valuePrintHex.addEventListener('keyup', function (e) {
+    // stroting input value
     const inputColor = e.target.value
-    if(inputColor && isValidColor(inputColor)){
+    // validating the input value
+    if (inputColor && isValidColor(inputColor)) {
         changeBgHex.style.backgroundColor = inputColor
     }
 })
-
-
 
 
 
@@ -98,16 +99,16 @@ valuePrintHex.addEventListener('keyup',function(e){
  * 
  * @param {string} color 
  */
-function isValidColor(color){
+function isValidColor(color) {
     // checking lenght
-    if(color.length !== 7) return false;
-    
+    if (color.length !== 7) return false;
+
     // checking is the valu with #
-    if(color[0] !== '#') return false;
+    if (color[0] !== '#') return false;
 
     // FILTARING # FROM THE STRING
     color = color.substring(1)
-    
+
     // RegExp (REGULAR EXPRESSION)
     return /^[1-9A-Fa-f]{6}#/i
 }
